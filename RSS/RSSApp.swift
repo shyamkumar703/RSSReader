be7eed1c76ea293dff7d3cd7974e9b8a090d5d03
Shyam Kumar <shyamkumar@berkeley.edu>
2023-01-11 16:34:04 -0800

    Initial Commit

---
+++ RSS/RSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSApp.swift
+//  RSS
+//
+//  Created by Shyam Kumar on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
